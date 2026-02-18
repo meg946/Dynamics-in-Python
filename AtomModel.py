245ffed56fcf858a9de2f5775e12dbfc3529777d
Gavin <michaelsg946@gmail.com>
2026-02-18 12:11:25 -0500

    Reformat AtomModel.py for readability
    Apply consistent formatting and style improvements across AtomModel.py: normalize spacing and indentation, reflow long lines, wrap long expressions and function arguments, adjust comment placement, and add/adjust blank lines for readability. No logic or behavior changes were made; this is purely a whitespace/style refactor.

--- AtomModel.py
+++ AtomModel.py
@@ -3,35 +3,37 @@ import random
 
 
 ##CONSTANTS##
-    #Unit Conversions
-eVc = 1.7826619216279e-36                                       #eV/c^2 convert to kilogram
-fm = 1e-15                                                      #femtometre to meter
-qSqrt3 = 1/sqrt(3)                                              #just to make it easier
+# Unit Conversions
+eVc = 1.7826619216279e-36  # eV/c^2 convert to kilogram
+fm = 1e-15  # femtometre to meter
+qSqrt3 = 1 / sqrt(3)  # just to make it easier
 
-    #Coulombs Constant
-SPEED_OF_LIGHT = 299792458                                      #Meters per Second
-MAGNETIC_CONSTANT = 1.25663706127e-6                            #Newtons per Ampere
-EPSILON_NAUGHT = 1/(MAGNETIC_CONSTANT * SPEED_OF_LIGHT**2)      #Farad per (1/meter)
-COULOMB_CONSTANT = 1/(4*pi) * (EPSILON_NAUGHT**-1)              #Newtons per (meters^2/Coulomb^2)
+# Coulombs Constant
+SPEED_OF_LIGHT = 299792458  # Meters per Second
+MAGNETIC_CONSTANT = 1.25663706127e-6  # Newtons per Ampere
+EPSILON_NAUGHT = 1 / (MAGNETIC_CONSTANT * SPEED_OF_LIGHT**2)  # Farad per (1/meter)
+COULOMB_CONSTANT = (
+    1 / (4 * pi) * (EPSILON_NAUGHT**-1)
+)  # Newtons per (meters^2/Coulomb^2)
 
-    #Elementary Charges
-E = 1.602176634e-19                                             #Coulombs
-UP_TYPE_QUARK_CHARGE = (2/3)*E                                  #Coulombs
-DOWN_TYPE_QUARK_CHARGE = (-1/3)*E                               #Coulombs
+# Elementary Charges
+E = 1.602176634e-19  # Coulombs
+UP_TYPE_QUARK_CHARGE = (2 / 3) * E  # Coulombs
+DOWN_TYPE_QUARK_CHARGE = (-1 / 3) * E  # Coulombs
 
-    #Elementary Mass
+# Elementary Mass
 UP_TYPE_QUARK_MASS = 2.16 * eVc
 DOWN_TYPE_QUARK_MASS = 4.7 * eVc
 ELECTRON_MASS = 0.51099895069 * eVc
 
-    #Elementary Radius
+# Elementary Radius
 PROTON_RADIUS = 0.84075 * fm
 NEUTRON_RADIUS = 0.8 * fm
 UP_TYPE_QUARK_RADIUS = 1e-4 * PROTON_RADIUS
 DOWN_TYPE_QUARK_RADIUS = 1e-4 * PROTON_RADIUS
-QUARK_VERTEX = 2*UP_TYPE_QUARK_RADIUS
+QUARK_VERTEX = 2 * UP_TYPE_QUARK_RADIUS
 
-    #Hadron Charges
+# Hadron Charges
 PROTON_CHARGE = UP_TYPE_QUARK_CHARGE * 2 + DOWN_TYPE_QUARK_CHARGE
 NEUTRON_CHARGE = UP_TYPE_QUARK_CHARGE + DOWN_TYPE_QUARK_CHARGE * 2
 ELECTRON_CHARGE = -E
@@ -39,25 +41,32 @@ ELECTRON_CHARGE = -E
 
 ##CLASSES##
 
-    #Elementary Classes
+
+# Elementary Classes
 class UpQuark:
-    def __init__(self,position):
+    def __init__(self, position):
         self.charge = UP_TYPE_QUARK_CHARGE
-        self.mass   = UP_TYPE_QUARK_MASS
+        self.mass = UP_TYPE_QUARK_MASS
         self.visual = sphere(pos=position, radius=UP_TYPE_QUARK_RADIUS, color=color.red)
+
+
 class DownQuark:
-    def __init__(self,position):
+    def __init__(self, position):
         self.charge = DOWN_TYPE_QUARK_CHARGE
-        self.mass   = DOWN_TYPE_QUARK_MASS
-        self.visual = sphere(pos=position, radius=DOWN_TYPE_QUARK_RADIUS, color=color.blue)
+        self.mass = DOWN_TYPE_QUARK_MASS
+        self.visual = sphere(
+            pos=position, radius=DOWN_TYPE_QUARK_RADIUS, color=color.blue
+        )
 
 
 ##Setup##
 
-ProtonSphere = sphere(pos=vector(0, 0, 0), radius=PROTON_RADIUS, color=color.white, opacity=0.5)
-q1 = UpQuark(vector(QUARK_VERTEX,QUARK_VERTEX,QUARK_VERTEX) * qSqrt3)
-q2 = UpQuark(vector(QUARK_VERTEX,-QUARK_VERTEX,-QUARK_VERTEX) * qSqrt3)
-q3 = DownQuark(vector(-QUARK_VERTEX,QUARK_VERTEX,-QUARK_VERTEX) * qSqrt3)
+ProtonSphere = sphere(
+    pos=vector(0, 0, 0), radius=PROTON_RADIUS, color=color.white, opacity=0.5
+)
+q1 = UpQuark(vector(QUARK_VERTEX, QUARK_VERTEX, QUARK_VERTEX) * qSqrt3)
+q2 = UpQuark(vector(QUARK_VERTEX, -QUARK_VERTEX, -QUARK_VERTEX) * qSqrt3)
+q3 = DownQuark(vector(-QUARK_VERTEX, QUARK_VERTEX, -QUARK_VERTEX) * qSqrt3)
 
 
-print(q1.charge+q2.charge+q3.charge)
+print(q1.charge + q2.charge + q3.charge)
